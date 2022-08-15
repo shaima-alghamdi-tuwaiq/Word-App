4a756ce459e912e7a6eec7813d4f1db06c4c52c6
shaima <s.alghamdi@safcsp.org.sa>
2022-08-15 12:18:55 +0300

    OptionMenu added

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,7 +16,11 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
@@ -25,7 +29,11 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
 class MainActivity : AppCompatActivity() {
+
     private lateinit var recyclerView: RecyclerView
+
+    // to keep track of which layout state
+    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,4 +47,57 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = LetterAdapter()
     }
 
+    // change different layout managers based on user clicks
+    private fun chooseLayout(){
+        if(isLinearLayoutManager){
+
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    // change menu item icon based on user clicks
+    private fun setIcon(menuItem: MenuItem?){
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+            ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        else
+            ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+
+    }
+
+    // Create option menu cmd + o , ctrl + o
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // inflate menu layout
+        menuInflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+
+        return true
+    }
+
+    // option menu item selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // return an action, behaviour
+        return when(item.itemId){
+            R.id.action_switch_layout ->
+            {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
+    }
 }
